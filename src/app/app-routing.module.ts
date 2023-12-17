6946cd72ed96f362eba431b3243e059806c16033
migue1angel <macf06200@gmail.com>
2023-12-17 12:03:05 -0500

    bloqueo de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,17 +10,17 @@ import { RoutesComponent } from './pages/routes/routes/routes.component';
 import { RoutesFormComponent } from './pages/routes/routes-form/routes-form.component';
 import { CustomerComponent } from './pages/customer/customer.component';
 import { DriverHomeComponent } from './pages/driver-home/driver-home.component';
-
+import{AuthGuard} from '../app/pages/auth/auth.guard'
 const routes: Routes = [
   { path: 'sign-up', component: SignUpComponent },
   { path: 'login', component: LoginComponent },
   { path: 'driver/:id', component: DriverFormComponent },
   { path: 'vehicle-form/:id', component: VehicleFormComponent },
-  {path:'register/:id', component:RegisterComponent},
-  {path: 'home/:id', component:HomeComponent}, 
-  {path: 'driverhome/:id', component:DriverHomeComponent}, 
-  {path: 'routes/:id', component:RoutesComponent}, 
-  {path: 'routes-form/:id', component:RoutesFormComponent}, 
+  {path:'register/:id',canActivate: [AuthGuard], component:RegisterComponent},
+  {path: 'home/:id',canActivate: [AuthGuard], component:HomeComponent}, 
+  {path: 'driverhome/:id',canActivate: [AuthGuard], component:DriverHomeComponent}, 
+  {path: 'routes/:id',canActivate: [AuthGuard], component:RoutesComponent}, 
+  {path: 'routes-form/:id',canActivate: [AuthGuard], component:RoutesFormComponent}, 
   {path: 'customer/:id', component: CustomerComponent},
   ]
 
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,40 +1,40 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-  user:any = {}
-  protected form: FormGroup;
-  constructor(private httpClient: HttpClient, private formBuilder: FormBuilder, private router: Router) {
+  user: any = {};
+  form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private router: Router, private authService: AuthService) {
     this.form = this.formBuilder.group({
       password: [null, [Validators.required, Validators.minLength(5)]],
       user: [null, [Validators.required]],
-    })
+    });
+  }
 
-  };
-
-  submitForm() {
-    this.form.markAllAsTouched()
+  async submitForm() {
+    this.form.markAllAsTouched();
     if (this.form.valid) {
       const data = this.form.value;
-      this.httpClient.post('http://localhost:3000/login', data).subscribe(response => {
-        this.user = response
-        if(this.user== null){
-          alert('usuario no encontrado')
-        }else(alert('Bienvenido'));
-        
-        if(this.user.role_id == 2){
-           this.router.navigate(['driverhome/'+ this.user.id_user])
-          }else if(this.user.role_id == 1){
-          this.router.navigate(['home/'+ this.user.id_user])
+      const user = await this.authService.login(data)
+      console.log(user);
+      if (user === null) {
+        alert('Usuario no encontrado');
+      } else {
+        alert('¡Bienvenido!');
+        if (user.role_id === 2) {
+          this.router.navigate(['driverhome', user.id_user]);
+        } else if (user.role_id === 1) {
+          this.router.navigate(['home', user.id_user]);
         }
-      });
+      }
     }
   }
 }
