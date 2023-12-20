d4caab6ed266e3bd9d96f38df0e0c0e62c77e9f7
j0n4t8an <morajonathanjm24@gmail.com>
2023-12-20 14:56:44 -0500

    update customer

--- src/app/pages/customer/customer.component.ts
+++ src/app/pages/customer/customer.component.ts
@@ -12,21 +12,31 @@ import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angula
 })
 export class CustomerComponent {
   userId = this.activatedRoute.snapshot.params['id']
-  customer: FormGroup;
+  customerForm: FormGroup;
+  customer: any = {};
+  
   constructor(protected  httpClient: HttpClient, protected  formBuilder: FormBuilder,protected  router: Router,private activatedRoute: ActivatedRoute) {
     if (activatedRoute.snapshot.params['id'] == '0'){}
-    this.customer = this.formBuilder.group({
+    if (activatedRoute.snapshot.params['id'] != '0') {
+      this.getCustomer()
+    }
+    this.customerForm = this.formBuilder.group({
       name: [null, Validators.required],
       lastname: [null, Validators.required],
       photo: [null, Validators.required],
       phone: [null, Validators.required],
-      user_id: [this.userId]
+      user_id: [this.userId],
     });
  }
 
  submit() {
-  if (this.customer.valid) {
-    const data = this.customer.value;
+  if (this.customer) {
+    this.customerUpdate();
+    alert('actualizado')
+    this.router.navigate(['home/'+ this.userId])
+  } else {
+  if (this.customerForm.valid) {
+    const data = this.customerForm.value;
     this.httpClient.post('http://localhost:3000/customer', data).subscribe(response => {
       alert('Usuario creado')
       this.router.navigate(['login'])
@@ -35,6 +45,30 @@ export class CustomerComponent {
       alert('Error al crear el usuario');
     });
   }
-  this.customer.reset();
 }
+}
+
+getCustomer(){
+  this.httpClient.get('http://localhost:3000/customer/'+ this.userId).subscribe((response: any) => {
+        this.customer = response;
+        console.log(this.customer);
+        this.customerForm.patchValue(this.customer)
+  })
+}
+
+customerUpdate() {
+  this.getCustomer();
+  this.customerForm;
+  if (this.customerForm.valid) {
+  const data = this.customerForm.value;
+  this.httpClient
+    .put('http://localhost:3000/customer/'+ this.customer.id_customer, data)
+    .subscribe((response) => {  
+    },(error) => {
+      console.log(error)
+      alert('Error al actualizar el datos del usuario');
+    });
+  }
+}
+
 }
